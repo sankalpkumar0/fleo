730854d125cd726d33868f466c94fc11849c7940
Vikrant-aidash <vikrant.gupta@aidash.com>
2022-04-17 03:09:33 +0530

    gg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Divider, Typography } from "@mui/material";
+import React from "react";
+import DataTable from "./commonComponents/dataTable/dataTable";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+function MyComponent() {
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(
+      "https://api.github.com/search/repositories?q=language:Javascript&sort=stars&order=desc"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <Typography
+          variant="h4"
+          sx={{
+            paddingBottom: "100px",
+            paddingTop: "50px",
+            paddingLeft: "700px",
+            paddingRight: "100px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          List of Github Repositories
+        </Typography>
+        <Divider />
+        <DataTable data={items} />;
+      </>
+    );
+  }
 }
-
-export default App;
+export default MyComponent;
---
+++ src/commonComponents/dataTable/dataTable.js
@@ -0,0 +1,111 @@
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+const columns = [
+  { id: "name", label: "Repo Name", minWidth: 170 },
+  { id: "description", label: "Description", minWidth: 100 },
+  {
+    id: "owner",
+    label: "Owner Name",
+    minWidth: 170,
+  },
+  {
+    id: "stargazers_count",
+    label: "Stars Count",
+    minWidth: 170,
+  },
+  {
+    id: "forks_count",
+    label: " Number of forks",
+    minWidth: 170,
+  },
+  { id: "language", label: "Language", minWidth: 170 },
+];
+export default function DataTable(props) {
+  const { data } = props;
+  console.log(data);
+  const rows = data.items;
+  console.log(rows);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const handleClick = (data) => {
+    window.location.href = `${data}`;
+  };
+
+  return (
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.code}
+                    onClick={() => handleClick(row.html_url)}
+                  >
+                    {columns.map((column) => {
+                      let value = row[column.id];
+                      if (column.id == "owner") {
+                        value = row.owner.login;
+                      }
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
